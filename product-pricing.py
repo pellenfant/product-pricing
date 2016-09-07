29338be7b9496a9cdb0c086a1ee4cc639f9e9ade
pellenfant <ellenfreider@Ellens-MacBook-Air.local>
2016-09-07 12:07:09 +0200

    Lagt till klasser för products
    Gjort om till klasser

--- product-pricing.py
+++ product-pricing.py
@@ -1,10 +1,31 @@
-robot = {"price": 900, "count": 2, "tax":1.25}
-#här har jag använt en dictionary för att samla all info om produkten robot.
+#print(900 * 2 * 1.25 + 100 * 1.06)
 
-book = {"price":100, "count":1, "tax": 1.06}
+# robot = {"price": 900, "count": 2, "tax":1.25}
+# #här har jag använt en dictionary för att samla all info om produkten robot.
 
-print(robot["price"]*robot["count"]*robot["tax"]+ book["price"]*book["count"]*book["tax"])
+# book = {"price":100, "count":1, "tax": 1.06}
 
+# print(robot["price"]*robot["count"]*robot["tax"]+ book["price"]*book["count"]*book["tax"])
 #för att använda värdena ur min dictionary skriver jag så här: 
 #jag åberopar listan (t.ex. robot9 genom att inleda med dictionaryns namn.
 #sedan väljer jag värden ur listan genom tt skriva dem inom hakparenteser och citattecken. 
+
+# class Product:
+# 	price = 0
+# 	count = 1
+# 	tax = 1
+
+# robot = Product()
+# robot.price = 900
+# robot.count = 2
+# robot.tax = 1.25
+
+# book = Product()
+# book.price = 100
+# book.count = 1
+# book.tax = 1.06
+
+	#det här är exempel på klasser
+	#för att printa och använda klasserna skriver jag så här:
+
+print (robot.price*robot.count*robot.tax+book.price*book.count*book.tax)
