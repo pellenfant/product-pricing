3029bfd5defc9ced20e66985b4d6eee82751a73a
pellenfant <ellenfreider@Ellens-MacBook-Air.local>
2016-09-07 12:28:34 +0200

    Lagt till en metod.

--- product-pricing.py
+++ product-pricing.py
@@ -10,13 +10,38 @@
 #jag åberopar listan (t.ex. robot9 genom att inleda med dictionaryns namn.
 #sedan väljer jag värden ur listan genom tt skriva dem inom hakparenteser och citattecken. 
 
+
+#skriv om med klass
+# class Product:
+# 	price = 0
+# 	count = 1
+# 	tax = 1
+	#det här är exempel på en klass
+
+#obs, det är viktigt att följande definitioner av produktens värden inte ligger indragna under klassen. 
+# robot = Product()
+# robot.price = 900
+# robot.count = 2
+# robot.tax = 1.25
+
+# book = Product()
+# book.price = 100
+# book.count = 1
+# book.tax = 1.06
+
+	#för att printa och använda klasserna skriver jag så här:
+
+#print (robot.price * robot.count * robot.tax + book.price * book.count * book.tax)
+
+#skriv om med klass OCH metod
 class Product:
 	price = 0
 	count = 1
 	tax = 1
-	#det här är exempel på en klass
 
-#obs, det är viktigt att följande definitioner av produktens värden inte ligger indragna under klassen. 
+	def price_with_tax(self):
+		return self.price * self.count * self.tax 
+
 robot = Product()
 robot.price = 900
 robot.count = 2
@@ -27,6 +52,16 @@ book.price = 100
 book.count = 1
 book.tax = 1.06
 
-	#för att printa och använda klasserna skriver jag så här:
+#här lägger jag till en metod (med def) vilket är en funktion inuti en klass
+#det metoden gör är att räkna ut priset med skatt för varje produkt. 
 
-print (robot.price * robot.count * robot.tax + book.price * book.count * book.tax)
+
+print(book.price_with_tax()+robot.price_with_tax())
+#när jag anropar metoder måste jag avsluta med en parentes (ibland tom)
+#det gör jag för att visa att det är ett metodanrop
+
+
+
+
+
+
