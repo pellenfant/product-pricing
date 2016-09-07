b921d848456483fe7063a56584a3d1ed6ba1a9ae
pellenfant <ellenfreider@Ellens-MacBook-Air.local>
2016-09-07 11:25:21 +0200

    Skrev om programmet med variabler.

--- product-pricing.py
+++ product-pricing.py
@@ -1,1 +1,14 @@
-print (900 * 2 * 1.25 + 100 * 1 * 1.06)
+
+robot_price = 900
+robot_count = 2
+robot_tax = 1.25 
+#det här är variabler
+#obs, det är viktigt att skriva decimaltal med punkt, inte kommatecken
+
+book_price = 100
+book_count = 1
+book_tax = 1.06 
+
+print(robot_price*robot_count*robot_tax+book_price*book_count*book_tax)
+#det här är ett exempel på hur variablerna kan användas. kommandot print ger en sträng till svar.
+
