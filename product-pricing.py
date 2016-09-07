f30fc3e690dc312810e5f630d787308bc97957f4
pellenfant <ellenfreider@Ellens-MacBook-Air.local>
2016-09-07 12:15:45 +0200

    Justerat buggar

--- product-pricing.py
+++ product-pricing.py
@@ -10,22 +10,23 @@
 #jag åberopar listan (t.ex. robot9 genom att inleda med dictionaryns namn.
 #sedan väljer jag värden ur listan genom tt skriva dem inom hakparenteser och citattecken. 
 
-# class Product:
-# 	price = 0
-# 	count = 1
-# 	tax = 1
+class Product:
+	price = 0
+	count = 1
+	tax = 1
+	#det här är exempel på en klass
 
-# robot = Product()
-# robot.price = 900
-# robot.count = 2
-# robot.tax = 1.25
+#obs, det är viktigt att följande definitioner av produktens värden inte ligger indragna under klassen. 
+robot = Product()
+robot.price = 900
+robot.count = 2
+robot.tax = 1.25
 
-# book = Product()
-# book.price = 100
-# book.count = 1
-# book.tax = 1.06
+book = Product()
+book.price = 100
+book.count = 1
+book.tax = 1.06
 
-	#det här är exempel på klasser
 	#för att printa och använda klasserna skriver jag så här:
 
-print (robot.price*robot.count*robot.tax+book.price*book.count*book.tax)
+print (robot.price * robot.count * robot.tax + book.price * book.count * book.tax)
