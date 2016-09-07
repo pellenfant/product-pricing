2b37123add25cce0acfbe30715f93e404608fe06
pellenfant <ellenfreider@Ellens-MacBook-Air.local>
2016-09-07 11:41:48 +0200

    Gjort anteckningar och gjort dictionaries.

--- product-pricing.py
+++ product-pricing.py
@@ -1,14 +1,10 @@
+robot = {"price": 900, "count": 2, "tax":1.25}
+#här har jag använt en dictionary för att samla all info om produkten robot.
 
-robot_price = 900
-robot_count = 2
-robot_tax = 1.25 
-#det här är variabler
-#obs, det är viktigt att skriva decimaltal med punkt, inte kommatecken
+book = {"price":100, "count":1, "tax": 1.06}
 
-book_price = 100
-book_count = 1
-book_tax = 1.06 
+print(robot["price"]*robot["count"]*robot["tax"]+ book["price"]*book["count"]*book["tax"])
 
-print(robot_price*robot_count*robot_tax+book_price*book_count*book_tax)
-#det här är ett exempel på hur variablerna kan användas. kommandot print ger en sträng till svar.
-
+#för att använda värdena ur min dictionary skriver jag så här: 
+#jag åberopar listan (t.ex. robot9 genom att inleda med dictionaryns namn.
+#sedan väljer jag värden ur listan genom tt skriva dem inom hakparenteser och citattecken. 
